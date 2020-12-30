322a4e45f566d8f0ec88820db6577f89a13b6610
shreyash dhanawade <68527969+shreyash00007@users.noreply.github.com>
2020-12-30 10:34:51 +0530

    Create Multipication.py

---
+++ Multipication.py
@@ -0,0 +1,6 @@
+  
+num1 = int(input("Enter the valve of  first no :-"))
+num2 = int(input("Enter the value of second no :-"))
+Multi = num1 + num2 
+
+print("The Multipication of two no is ",Multi)
